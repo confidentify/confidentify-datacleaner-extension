76bb351fafc19dfcd6f3f6a0e3f595452f04a0e1
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2020-03-23 21:46:27 +0100

    Added email transformer. Improved error handling.

--- src/main/java/com/confidentify/datacleaner/components/EmailTransformer.java
+++ src/main/java/com/confidentify/datacleaner/components/EmailTransformer.java
@@ -20,17 +20,16 @@ import com.confidentify.client.ApiClient;
 import com.confidentify.client.ApiException;
 import com.confidentify.client.ApiResponse;
 import com.confidentify.client.api.ProcessApi;
-import com.confidentify.client.model.NameRequest;
-import com.confidentify.client.model.NameRequestRecord;
-import com.confidentify.client.model.NameResponse;
-import com.confidentify.client.model.NameResponseRecord;
-import com.confidentify.client.model.NameResponseRecordEntity;
+import com.confidentify.client.model.EmailRequest;
+import com.confidentify.client.model.EmailRequestRecord;
+import com.confidentify.client.model.EmailResponse;
+import com.confidentify.client.model.EmailResponseRecord;
 import com.confidentify.client.model.ProcessorOutcome;
 import com.confidentify.datacleaner.ApiClientProvider;
 
-@Named("Person name (conf·ident·ify)")
+@Named("Email (conf·ident·ify)")
 @Categorized(value = ConfidentifyCategory.class, superCategory = ImproveSuperCategory.class)
-public class PersonNameTransformer extends BatchTransformer {
+public class EmailTransformer extends BatchTransformer {
 
     private final ApiClientProvider apiClientProvider = ApiClientProvider.get();
 
@@ -42,12 +41,12 @@ public class PersonNameTransformer extends BatchTransformer {
     String password;
 
     @Configured
-    InputColumn<String> nameColumn;
+    InputColumn<String> emailColumn;
 
     @Override
     public OutputColumns getOutputColumns() {
-        final String[] columnNames = { "Entities", "Verdict", "Info", "Warn" };
-        final Class<?>[] columnTypes = { List.class, String.class, List.class, List.class };
+        final String[] columnNames = { "Email (formatted)", "Email (simplified)", "Email (suggested)", "Verdict", "Info", "Warn" };
+        final Class<?>[] columnTypes = { String.class, String.class, String.class, String.class, List.class, List.class };
         return new OutputColumns(columnNames, columnTypes);
     }
 
@@ -55,34 +54,36 @@ public class PersonNameTransformer extends BatchTransformer {
     public void map(BatchSource<InputRow> source, BatchSink<Object[]> sink) {
         final int count = source.size();
 
-        final List<NameRequestRecord> requestRecords = new ArrayList<>(count);
+        final List<EmailRequestRecord> requestRecords = new ArrayList<>(count);
 
         for (int i = 0; i < count; i++) {
             final InputRow inputRow = source.getInput(i);
-            final String inputName = inputRow.getValue(nameColumn);
+            final String inputEmail = inputRow.getValue(emailColumn);
             final String recordId = "r_" + i;
-            final NameRequestRecord requestRecord = new NameRequestRecord().id(recordId).name(inputName);
+            final EmailRequestRecord requestRecord = new EmailRequestRecord().id(recordId).email(inputEmail);
             requestRecords.add(requestRecord);
         }
 
-        final NameRequest nameRequest = new NameRequest();
-        nameRequest.setRecords(requestRecords);
+        final EmailRequest emailRequest = new EmailRequest();
+        emailRequest.setRecords(requestRecords);
 
         final ApiClient apiClient = apiClientProvider.getApiClient(username, password);
         final ProcessApi api = new ProcessApi(apiClient);
         try {
-            final ApiResponse<NameResponse> response = api.personNamePostWithHttpInfo(nameRequest);
-            final NameResponse data = response.getData();
-            final List<NameResponseRecord> records = data.getRecords();
+            final ApiResponse<EmailResponse> response = api.emailPostWithHttpInfo(emailRequest);
+            final EmailResponse data = response.getData();
+            final List<EmailResponseRecord> records = data.getRecords();
             for (int i = 0; i < count; i++) {
-                final NameResponseRecord responseRecord = records.get(i);
+                final EmailResponseRecord responseRecord = records.get(i);
                 assert ("r_" + i).equals(responseRecord.getId());
                 final ProcessorOutcome outcome = responseRecord.getOutcome();
-                final List<NameResponseRecordEntity> entities = responseRecord.getEntities();
+                final String emailFormatted = responseRecord.getEmailFormatted();
+                final String emailSimplified = responseRecord.getEmailSimplified();
+                final String emailSuggested = responseRecord.getEmailSuggested();
                 final String verdict = outcome == null ? "" : outcome.getVerdict() == null ? "" : outcome.getVerdict().getValue();
                 final List<String> info = outcome == null ? null : outcome.getInfo();
                 final List<String> warn = outcome == null ? null : outcome.getWarn();
-                sink.setOutput(i, new Object[] { entities, verdict, info, warn });
+                sink.setOutput(i, new Object[] { emailFormatted, emailSimplified, emailSuggested, verdict, info, warn });
             }
         } catch (ApiException e) {
             throw new RuntimeException(e);
